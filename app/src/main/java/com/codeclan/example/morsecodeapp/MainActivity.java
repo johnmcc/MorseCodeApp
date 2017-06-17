f9cf377ca40c464a8582ae42afbdad52a98c11fb
John McCollum <johnvmccollum@gmail.com>
2017-06-17 08:56:53 +0100

    Added player object to main activity

--- app/src/main/java/com/codeclan/example/morsecodeapp/MainActivity.java
+++ app/src/main/java/com/codeclan/example/morsecodeapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.morsecodeapp;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Encoder encoder;
+    MorsePlayer player;
     TextView resultTv;
     EditText toEncode;
 
@@ -19,14 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         encoder = new Encoder();
+        player = new MorsePlayer();
         resultTv = (TextView) findViewById(R.id.result);
         toEncode = (EditText) findViewById(R.id.to_encode);
     }
 
-    public void encode(View view) {
+    public void updateUI(){
         String toEncode = this.toEncode.getText().toString();
         encoder.setPhrase(toEncode);
         String result = encoder.encode();
         resultTv.setText(result);
+    }
+
+    public void encode(View view) {
+        updateUI();
+    }
+
+    public void play(View view) {
+        updateUI();
+
+        player.setMorse(encoder.encode());
+        player.play();
+
     }
 }
---
+++ app/src/main/java/com/codeclan/example/morsecodeapp/MorsePlayer.java
@@ -0,0 +1,61 @@
+package com.codeclan.example.morsecodeapp;
+
+/**
+ * Created by user on 16/06/2017.
+ */
+
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+
+public class MorsePlayer {
+    String morse;
+    ToneGenerator tg;
+
+    public MorsePlayer() {
+        tg = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+    }
+
+    public String getMorse() {
+        return morse;
+    }
+
+    public void setMorse(String morse) {
+        this.morse = morse;
+    }
+
+    private void sleep(int duration){
+        try {
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play(){
+        String words[] = this.morse.split(" / ");
+        Character dit = '.';
+        Character dah = '-';
+
+        for(String word:words){
+            String splitted[] = word.split(" ");
+
+            for(String letter:splitted){
+                if(letter.equals(" ")){
+
+                }else{
+                    int duration = 0;
+                    for(Character c:letter.toCharArray()){
+                        if(c == dit){
+                            duration = 100;
+                        }else if(c == dah){
+                            duration = 350;
+                        }
+                        tg.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, duration);
+                        sleep(duration + 50);
+                    }
+                }
+                sleep(400);
+            }
+        }
+    }
+}
